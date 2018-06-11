d1bcb2bccbd99697af1cacc4416f14b7dcf37236
Елиза Стоянова <elizastoqnova@macbook-air.local>
2018-06-11 18:07:03 +0300

    Initial Commit

---
+++ MyOffersEli/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyOffersEli
+//
+//  Created by Елиза Стоянова on 11.06.18.
+//  Copyright © 2018 Елиза Стоянова. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyOffersEli/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyOffersEli
+//
+//  Created by Елиза Стоянова on 11.06.18.
+//  Copyright © 2018 Елиза Стоянова. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
